05ebc4fae03f6cf0d7dbed7cafb17d4a7f28e76c
yxy245652 <yxy245652@antfin.com>
2022-09-22 17:46:39 +0800

    fix: 优化样式引入

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
@@ -5,8 +6,8 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link rel="stylesheet" href="https://unpkg.com/@douyinfe/semi-ui@2.1.4/dist/css/semi.css"></link>
-                <link rel="stylesheet" href="https://unpkg.com/@douyinfe/semi-icons@latest/dist/css/semi-icons.css"></link>
+                {/* <link rel="stylesheet" href="https://unpkg.com/@douyinfe/semi-ui@2.1.4/dist/css/semi.css"></link>
+                <link rel="stylesheet" href="https://unpkg.com/@douyinfe/semi-icons@latest/dist/css/semi-icons.css"></link> */}
             </Head>
             <body>
                 <Main />
